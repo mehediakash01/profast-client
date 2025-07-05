ae89c73a45db84bbae8f2be1b0c4da897f94b5be
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-07-05 23:35:17 +0600

    set up tenstack query

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,17 @@ import { RouterProvider } from "react-router";
 import AuthProvider from "./Context/AuthProvider";
 import { router } from "./Router/router";
 import { Toaster } from "react-hot-toast";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
-       <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+          <Toaster />
+      </QueryClientProvider>
+
+    
     </AuthProvider>
   </StrictMode>
 );
