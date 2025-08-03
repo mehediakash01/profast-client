97762825d6dbf04d27f03e57f5201cb96dc7dd73
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-08-03 23:00:25 +0600

    sending firebase token for jwt

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,12 +1,36 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import { useEffect } from "react";
+import useAuthContext from "./useAuthContext";
+
 const axiosSecure = axios.create({
   baseURL: `http://localhost:3000/`,
-
 });
+
 const useAxiosSecure = () => {
-    return axiosSecure
-      
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    const accessToken = user?.accessToken;
+
+    const interceptor = axiosSecure.interceptors.request.use(
+      (config) => {
+        if (accessToken) {
+          config.headers.Authorization = `Bearer ${accessToken}`;
+          console.log("🔐 Token attached:", config.headers.Authorization);
+        }
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(interceptor);
+    };
+  }, [user?.accessToken]);
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
